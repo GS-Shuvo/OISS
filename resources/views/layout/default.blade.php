3b969a2f866f7ea8f735ebd1f59c32ee316fccff
GS-Shuvo <shuvosikder1987@gmail.com>
2023-03-03 03:52:13 +0600

    initial stage done

---
+++ resources/views/layout/default.blade.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html>
+<head>
+   @include('includes.head')
+</head>
+<body>
+<div class="container-scroller">
+    @include('includes.header')
+    <div class="container-fluid page-body-wrapper">
+      @include('includes.rightSideBar')
+      
+     @include('includes.navbar')
+      <div class="main-panel">
+        <div class="content-wrapper">
+            @yield('content')
+        </div>
+        <!-- content-wrapper ends -->
+        @include('includes.footer')
+      </div>
+      <!-- main-panel ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+
+  <!-- plugins:js -->
+  <script src="{{asset('assets/vendors/js/vendor.bundle.base.js')}}"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page -->
+  <script src="{{asset('assets/vendors/chart.js/Chart.min.js')}}"></script>
+  <script src="{{asset('assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js')}}"></script>
+  <script src="{{asset('assets/vendors/progressbar.js/progressbar.min.js')}}"></script>
+
+  <!-- End plugin js for this page -->
+  <!-- inject:js -->
+  <script src="{{asset('assets/js/off-canvas.js')}}"></script>
+  <script src="{{asset('assets/js/hoverable-collapse.js')}}"></script>
+  <script src="{{asset('assets/js/template.js')}}"></script>
+  <script src="{{asset('assets/js/settings.js')}}"></script>
+  <script src="{{asset('assets/js/todolist.js')}}"></script>
+  <!-- endinject -->
+  <!-- Custom js for this page-->
+  <script src="{{asset('assets/js/jquery.cookie.js')}}" type="text/javascript"></script>
+  <script src="{{asset('assets/js/dashboard.js')}}"></script>
+  <script src="{{asset('assets/js/Chart.roundedBarCharts.js')}}"></script>
+  <!-- End custom js for this page-->
+</body>
+</html>
